f6726aeace91f4dd80d57e408a0f52fa5d1daef9
dic1911 <d69873@gmail.com>
2025-02-06 13:54:24 +0800

    fix: add missing import

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
@@ -11,6 +11,7 @@ import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.ApplicationLoader;
 import org.telegram.messenger.BuildVars;
 import org.telegram.messenger.LocaleController;
+import org.telegram.messenger.MediaDataController;
 import org.telegram.messenger.MessagesController;
 import org.telegram.messenger.R;
 import org.telegram.messenger.SharedConfig;
